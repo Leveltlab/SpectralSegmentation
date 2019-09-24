116e9146b9760279c38e8f963a148ec6e57f6596
Leandernu <leanderdk@gmail.com>
2019-09-24 17:57:59 +0200

    getspectrois update & backgroundImgSbx added

---
+++ spectral/dep/BackgroundImgSbx.m
@@ -0,0 +1,205 @@
+function [BImgMax, BImgAverage] = BackgroundImgSbx(varargin)
+% Create GFP channel images from long recording
+% 
+% [BImgMax, BImgAverage] = BackgroundImgSbx(sbxName, spigfileName);
+% 
+% Optional input: 1. sbx file name (string) (with foldername if necissary)
+%                 2. spsig file name to save the images to
+%
+% In case only one input is given (the sbx filename), the spectral file is
+% assumed to be present in the same folder with the usual naming scheme.
+% In case no input is given the file names will be requested.
+% 
+% The background images are created by taking a requested amount of chunks
+% from the data, each chunk evenly spaced in time from the next chunk. 
+% A chunk averages a small number of frames to reduce noise, 
+% and is then max projected onto the  image that is being build up by
+% the different chunks.
+%
+% Leander de Kraker
+% 2018-10-17
+%
+
+%% You can also execute this as a script
+
+nchunks = 300; % How many pieces of the data to take
+nframes = 25; % How many frames are those pieces long
+
+if exist('varargin', 'var') && nargin == 2 % In case both sbx and spsig file name are given
+    % When called as a function, make sure the normcorr sbx file is being used
+    sbxName = varargin{1};
+    spsigName = varargin{2};
+    % find if the filename has to be edited, mat and SPSIG has to be removed
+    delimeterPos = regexp(sbxName,{'_SPSIG','.mat','sbx'}); 
+    delimeterPos = delimeterPos(~cellfun('isempty',delimeterPos));
+    if ~isempty(delimeterPos)
+    	sbxName = sbxName(1:delimeterPos{1}-1);
+    end
+    plotter = false;
+    
+elseif exist('varargin', 'var') && nargin == 1 % in case only sbx filename is given
+    sbxName = varargin{1};
+    % find if the filename has to be edited, mat and SPSIG has to be removed
+    delimeterPos = regexp(sbxName,{'_SPSIG','.mat', 'sbx'}); 
+    delimeterPos = delimeterPos(~cellfun('isempty',delimeterPos));
+    if ~isempty(delimeterPos)
+    	sbxName = sbxName(1:delimeterPos{1}-1);
+    end
+    spsigName = [sbxName '_SPSIG'];
+    plotter = false;
+    
+else  % If not called/ executing with input, request file
+    [fn , pn] = uigetfile('*.sbx','get the sbx file to read the data from');
+    sbxName = [pn fn(1:end-4)];
+    if fn == 0
+        return
+    end
+    [fn , pn] = uigetfile('*_SPSIG.mat','get the SPSIG file to save the images to');
+    spsigName = [pn fn(1:end-4)];
+    plotter = true;
+    
+end
+
+sbxread(sbxName, 0,1);
+global info
+fprintf('Creating background image for %s\n', sbxName)
+
+% Activate much tighter subplots
+% [subplot margin top&side],[figure bottomspace,topspace],[leftspace,rightspace]
+subplot = @(m,n,p) subtightplot (m, n, p, [0.075 0.005], [0.05 0.07], [0.05 0.01]);
+
+colors = cmapL([1     1    1;...
+                1     1    0.75;...
+                0.75  1    0.5;...
+                0.25  0.75 0.5;...
+                0     0.25 0.5;...
+                0     0    0], 256);
+
+nf = 250; % number of frames for example images
+img = sbxread(sbxName, 0,nf);
+
+nchannels = size(img, 3);
+fprintf('%d color channel(s) seem(s) present\n', nchannels)
+
+% Create example frames to check between beginning and end of recording
+imgAv1 = mean(squeeze(img(:,:,1,:)),3); 
+img = sbxread(sbxName, info.max_idx-nf-1, nf);
+imgAv2 = mean(squeeze(img(:,:,1,:)),3);
+
+% Take an average using different times in the recording.
+BImgMax = zeros(size(imgAv1));
+BImgAverage = zeros(size(imgAv1));
+if nchannels >= 2
+    BImgMaxC2 = zeros(size(imgAv1));
+    BImgAverageC2 = zeros(size(imgAv1));
+end
+
+% The pieces of data are taken at equal intervals
+chunkStart = floor(linspace(0, info.max_idx-nframes-1, nchunks));
+
+if plotter
+    figure('units','normalized','position',[0.25 0.2 0.5 0.4]);
+    tic
+end
+
+% calculate the images
+for i = 1:nchunks
+    chunkImage = sbxread(sbxName, chunkStart(i), nframes);
+    chunkImage2 = mean(squeeze(chunkImage(:,:,1,:)),3);
+    BImgMax = max(BImgMax, chunkImage2);
+    BImgAverage = BImgAverage + chunkImage2;
+
+    if mod(i,10)==0 && plotter
+        subplot(1,2,1),
+        imagesc(BImgMax); colorbar
+        title(sprintf('MaxProjection: iteration %d/%d. used %.3f%% of sbx file: time %.2f sec',...
+            i, nchunks, (nframes*i./info.max_idx*100), toc))
+        subplot(1,2,2),
+        imagesc(BImgAverage/i); colorbar
+        title(sprintf('average projection: maxval=%.1f',max(BImgAverage(:))))
+        colormap(colors)
+        pause(0.001)
+        tic
+    end
+    
+    if nchannels >= 2 % Create a background image for the 2nd color channel
+        chunkImage2 = mean(squeeze(chunkImage(:,:,2,:)),3);
+        BImgMaxC2 = max(BImgMaxC2, chunkImage2);
+        BImgAverageC2 = BImgAverageC2 + chunkImage2;
+    end
+end
+BImgAverage = BImgAverage/nchunks;
+
+% Fix horizontal alignment, error from incorrect bi-directional scanning
+% BImgMaxLines = BImgMax;
+% BImgMaxLines(1:2:end, 1:end-1) = BImgMaxLines(1:2:end, 2:end);
+% BImgAvLines = BImgAverage;
+% BImgAvLines(1:2:end, 1:end-1) = BImgAvLines(1:2:end, 2:end);
+
+figure('units','normalized','position',[0.01 0.1 0.25 0.8]);
+h = gobjects(4,1);
+h(1) = subplot(2,1,1);
+imagesc(log1p(imgAv1))
+title(sprintf('mean projection of first %d frames', nf))
+h(2) = subplot(2,1,2);
+imagesc(log1p(imgAv2));
+title(sprintf('mean projection of last %d frames', nf))
+colormap(colors)
+figtitle(sbxName)
+
+figure('units','normalized','position',[0.26 0.1 0.25 0.4]);
+h(3) = subplot(1,1,1);
+imagesc(log1p(BImgAverage))
+colormap(colors)
+title(sprintf('average %d chunks of %d frames = %d frames',nchunks, nframes, nchunks*nframes))
+
+figure('units','normalized','position',[0.26 0.5 0.25 0.4]);
+h(4) = subplot(1,1,1);
+imagesc(log1p(BImgMax))
+colormap(colors)
+title(sprintf('max projections of %d chunks of %d averaged frames = %d frames',...
+    nchunks, nframes, nchunks*nframes))
+% print(gcf, [fn(1:end-18) 'img_averageFluorescence.png'],'-dpng', '-r400')
+
+linkaxes(h, 'xy')
+
+
+
+if nchannels >= 2
+    figure('units','normalized','position',[0.53 0.1 0.3 0.8]);
+    h = gobjects(2,1);
+    h(1) = subplot(2,1,1);
+    imagesc(log1p(BImgAverageC2))
+    title(sprintf('average %d chunks of %d frames = %d frames',nchunks, nframes, nchunks*nframes))
+    h(2) = subplot(2,1,2);
+    imagesc(log1p(BImgMaxC2))
+    title(sprintf('max projections of %d chunks of %d averaged frames = %d frames',...
+        nchunks, nframes, nchunks*nframes))
+    colormap([colors(:,2) colors(:,3) colors(:,1)]) % Custom shuffled black-greenish-white colormap
+    figtitle(sbxName)
+    linkaxes(h, 'xy')
+end
+
+infoimg.percent = nframes*i./info.max_idx*100;
+infoimg.framesPerChunk = nframes;
+infoimg.nchunks = nchunks;
+infoimg.comment = {'BImgMax= maximum projection';'BImgAverage= average projection';...
+    'BImg= low frequency spectral components';...
+    'SpatialCorr= signal correlation per pixel with ROI brightest pixel'};
+if nchannels >= 2
+    infoimg.comment(end+1) = {'BImgMaxC2 = maximum projection of 2nd color channels'};
+    infoimg.comment(end+1) = {'BImgAverageC2 = average projection of 2nd color channels'};
+end
+fprintf('done calculating :)\n')
+
+
+%% The end: save it
+
+save(spsigName, 'BImgMax','BImgAverage','infoimg','-append')
+if nchannels >= 2
+    save(spsigName, 'BImgMaxC2','BImgAverageC2','-append')
+end
+fprintf('saved background images\n\n')
+
+end
+
--- spectral/getSpectrois.m
+++ spectral/getSpectrois.m
@@ -1,10 +1,33 @@
 %
-% Get ROIs from cross spectral components
-% Edit the ROIs with the RoiRejecterGUI
-% 
+% get rois from cross spectral components
+%
+% output: Mask: an image with the same size as BImg, with numbers that
+%               denote which ROI is present at which pixel
+%         
+%         PP: struct with ROI information
+%         PP.Cnt: The number of ROIs
+%         PP.Con contours: for every ROI defined by x and y values
+%         PP.A: area of the contour in pixels
+%         PP.SpecProfile: the mean values of spectral density for every
+%                         frequency in every ROI
+%         PP.peakFreq: Which frequency has the highest spectral power value
+%         PP.P: Values for every ROI
+%         PP.P: row 1: x values of the pixel with highest value in the BImg
+%         PP.P: row 2: y values of the pixel with highest value in the BImg
+%         PP.P: row 3: the minimum value of BImg in each ROI
+%         PP.P: row 4: the maximum value of BImg in each ROI
+%         PP.P: row 5: The average correlation value of the fluorescence
+%                      signal from the 'seed'/highest value pixel with the
+%                      signal from all other pixels in the ROI
+%
+%         SpatialCorr:
+%         spar: Spectral PARameters used to find the ROIs
+%
 %
 
- %% Load and Process Spectral Images:  load('SPic.mat')
+
+%% Load and Process Spectral Images:  load('SPic.mat')
+
 [fn, pn] = uigetfile('*_SPSIG.mat');
 filename = [pn fn];
 fprintf('\nloading...')
@@ -12,10 +35,6 @@ load(filename)
 Transfile = [filename(1:end-9) 'Trans.dat'];
 [sbxt, ~, freq] = transmemap(Transfile);
 fprintf('\nloaded\n')
-
-% Activate much tighter subplots
-% [subplot margin top&side],[figure bottomspace,topspace],[leftspace,rightspace]
-subplot = @(m,n,p) subtightplot (m, n, p, [0.01 0.01], [0.06 0.05], [0.06 0.05]);
 
 % obtain average spectral density for each image: decays exponentially
 imgStack = log(SPic(:,:,2:end));
@@ -27,19 +46,23 @@ if vals(1)==-inf
 end
 imgStackT = permute(imgStack,[2 1 3]); % transpose the SPic variable so it's same as BImg
 
-
-Slow = find(Sax <= 0.2);
+cutOffHz = 0.2;
+Slow = find(Sax <= cutOffHz);
 Spect = imgStack(:,:,Slow);
-
-%Fast = find(Sax > 0.5 & Sax <= 1.0);
-%Sfast = mean(Imgstack(:,:,Fast),3);
-%Spect = cat(3, Spect, Sfigure, fast);
 Spect = permute(Spect, [2 1 3]); %transposes the images,
 Spect = setminlevel(Spect); %set minimum level to zero
 
+
+% Activate much tighter subplots
+% [subplot margin top&side],[figure bottomspace,topspace],[leftspace,rightspace]
+subplot = @(m,n,p) subtightplot (m, n, p, [0.075 0.005], [0.05 0.07], [0.09 0.01]);
+
 BImg = max(Spect, [], 3);
-figure; subplot(1,1,1)
-imagesc(BImg), caxis(prctile(BImg(:), [0.01 99.9])), hold on, colorbar
+
+figure('units','normalized','position',[0.51 0.1 0.25 0.4]);
+subplot(1,1,1)
+imagesc(BImg), caxis(prctile(BImg(:), [0.01 99.9])), hold on
+title(sprintf('spectral components max projection <%.1fHz', cutOffHz))
 
 
 %% check how the spectral looks (press any key to advance, ctrl+c to stop)
@@ -57,7 +80,7 @@ nSpec = size(imgStackT,3);
 % A spectral image for all the different frequencies
 colors = flip(jet(nSpec));
 norma = false; % normalize each frequency?
-figure
+figure('units','normalized','position',[0.51 0.5 0.25 0.4]);
 subplot(1,1,1)
 SPicTCell = squeeze(num2cell(imgStackT, [1 2]));
 RGBspectral = CreateRGB2(SPicTCell, 1:nSpec, colors, norma);
@@ -71,53 +94,8 @@ h.Ticks = h.Ticks; % prevent ticks from changing
 h.TickLabels = num2str(hTicks', '%2.2f');
 ylabel(h, 'spectral frequency', 'FontSize',12)
 
-
-%% Plot the maximum and average projection for comparison with spectral
-
-if exist('BImgA','var') && ~exist('BImgMax','var')
-    BImgMax = BImgA;
-end
-
-% Plot and save picture
-hand2 = figure;
-colors = cmapL([1     1    1;...
-                1     1    0.75;...
-                0.75  1    0.5;...
-                0.25  0.75 0.5;...
-                0     0.25 0.5;...
-                0     0    0], 256);
-subplot(1,1,1)
-imagesc(log1p(BImgMax), prctile(log1p(BImgMax(:)), [0.01 99.8]))
-colormap(colors);
-title('log scaled maximum fluorescence image')
-h = colorbar;
-ylabel(h, 'maximum fluorescence', 'FontSize',12)
-% Save picture
-print(hand2, [fn(1:end-18) 'img_maximumFluorescence.png'],'-dpng', '-r400')
-
-if exist('BImgAverage','var') % Plot average fluorescence
-    colormap(colors);
-    hand3 = figure;
-    subplot(1,1,1)
-    imagesc(log1p(BImgAverage), prctile(log1p(BImgAverage(:)), [0.01 99.8]))
-    colormap(colors);
-    title('log scaled average fluorescence image')
-    h = colorbar;
-    ylabel(h, 'average fluorescence', 'FontSize',12)
-    % Save picture
-    print(hand3, [fn(1:end-18) 'img_averageFluorescence.png'],'-dpng', '-r400')
-end
-            
-vals = prctile(BImgMax(:),99.5);  % Cut off the highest 0.5% of values
-BImgMax(BImgMax>vals) = vals;
-
-figure;
-imagesc(CreateRGB({Spect(:,:,1), Spect(:,:,end), log1p(BImgMax)}, 'r b g'))
-title('red: lowest frequency, blue highest low frequency, green: max projection')
-
-
- %% ROI selection based on cross spectral density
- %find ROIs with higest local maxima in low frequency component images
+%% ROI selection based on cross spectral density
+% find ROIs with higest local maxima in low frequency component images
 
 PP = [];
 PP.Cnt = 0; 
@@ -131,7 +109,7 @@ for i = 1:dim(3)
     fprintf('iteration %2d/%2d: ', i, dim(3))
     Img = Spect(:,:,i); 
     Img(Mask>0) = 0;
-    figure(2), hold off,imagesc(Img), colormap gray, hold on %, caxis([h0 h1])     
+    figure(1), hold off,imagesc(Img), colormap gray, hold on %, caxis([h0 h1])     
     [PP, Mask, SpatialCorr] = roisfromlocalmax(Img, PP, Mask, spar, sbxt, freq, SpatialCorr);   
 
     str = sprintf('number of ROIs found: %5d. time elapsed = %.2fminutes\n', PP.Cnt, toc/60);
@@ -145,7 +123,7 @@ for i = 1:dim(3)
     pause(0.01)
 end
 
-%swap x and y values, otherwise positions are not correct in Displayrois
+% swap x and y values, otherwise positions are not correct in Displayrois
 P = PP.P; % plot(x,y), plots at the position (y, x) in an image
 PP.P(1,:) = P(2,:);
 PP.P(2,:) = P(1,:);
@@ -158,8 +136,8 @@ for i = 1:PP.Cnt
     %also redefine peak maxima based on projected maxima in BImg
     ROIi = Mask == i; 
     npixels = sum(ROIi(:)); % number of pixels for this ROI
-    PP.P(3,i) = max(BImg(ROIi)); %max
-    PP.P(4,i) = min(BImg(ROIi)); %min
+    PP.P(3,i) = max(BImg(ROIi)); % maximum
+    PP.P(4,i) = min(BImg(ROIi)); % minimum
     
     % Retrieve the average spectral profile of the ROI
     specProfile = reshape(imgStackT(Mask3D==i), [npixels, nSpec]);
@@ -173,58 +151,5 @@ end
 [~, peakFreq] = max(PP.SpecProfile);
 PP.peakFreq = Sax(peakFreq);
 
-%% Display  ROIS
- 
-%contours
-hold on
-Con = PP.Con;
-for k = 1:PP.Cnt
-    vx = Con(k).x;
-    vy = Con(k).y;
-    plot(vx, vy, 'r')
-end
-
-
-%SpatialCorr
-figure, imagesc(SpatialCorr)
-hold on
-Con = PP.Con;
-for k = 1:PP.Cnt
-    vx = Con(k).x;
-    vy = Con(k).y;
-    plot(vx, vy, 'r')
-end
-
-% mask
-cm = flipud(parula(max(Mask(:))));
-cm(1,:) = [0.5, 0.5, 0.5];
-figure, imagesc(Mask), colormap(cm)
-for i = 1:PP.Cnt
-    [iy,ix] = find(Mask == i);
-    mx = median(ix);
-    my = median(iy);
-    text(mx, my, num2str(i), 'color', 'w')
-end
-
-
-%% Reject and add ROIs, & look at signal with UI!
-RoiRejecterGUI(Mask, PP, SPic, Transfile, Sax)
-
-
-%% Calculate power spectral density function for each roi and then save
-ln = length(Sax);
-Ps = zeros(ln,PP.Cnt);
-Mt = Mask';
-SPImg = reshape(imgStack, [], ln);
-for i = 1:PP.Cnt
-    ichan = find(Mt(:)== i);
-    Ps(:,i) = mean(SPImg(ichan,:)); %and averge this selection 
-end
-
-% Save rois
-% save(filename, 'PP', 'Mask', 'BImg', 'Ps', 'spar', 'SpatialCorr', '-append')
-
-save(filename, 'PP', 'Mask', 'BImg', 'Ps', 'spar', '-append')
+save(filename, 'PP', 'Mask', 'BImg', 'spar', 'SpatialCorr', '-append')
 fprintf('saved.\n')
-
-
