7ebc25ebab1dd2cbded417f453f129dad5f9b18f
Leanderrr-hel2p <leanderdk@gmail.com>
2021-07-24 17:45:07 +0200

    Better filename guessing for getSpectrois
    - The filename gets guessed in a better way
    - And that change doesn't crash the code when running as a function..

--- spectral/getSpectrois.m
+++ spectral/getSpectrois.m
@@ -63,9 +63,11 @@ end
 fprintf('\nloading...')
 load(filenameSPSIG, 'SPic', 'Sax')
 
-filenameTrans = [filenameSPSIG(1:end-9) 'DecTrans.dat'];
+sfn = regexp(filenameSPSIG,'SPSIG', 'split');
+filenameTrans = [sfn{1} 'DecTrans.dat'];
+
 if ~isfile(filenameTrans) %Using decimated data
-    filenameTrans = [filenameSPSIG(1:end-9) 'Trans.dat'];
+    filenameTrans = [sfn{1} 'Trans.dat'];
     if ~isfile(filenameTrans) % Resort to manual selection of transposed data
         [fnTrans, pnTrans] = uigetfile('*.dat', 'Where is the transposed data file (prefering decimated data)?');
         if fnTrans == 0
