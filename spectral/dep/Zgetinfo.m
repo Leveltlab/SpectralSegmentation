6dfbab3662bc9dff426c8915f9bbe45ee9cf6542
Chris van der Togt <c.vandertogt@nin.knaw.nl>
2022-06-29 15:33:19 +0200

    Update Zgetinfo.m

--- spectral/dep/Zgetinfo.m
+++ spectral/dep/Zgetinfo.m
@@ -1,9 +1,14 @@
 function [Dim, Freq, Type] = Zgetinfo(strfn)
 
-fid = fopen(strfn);
-Header = fgetl(fid);
-idx = 1:min(length(Header), 100); % 
-strh = strsplit(Header(idx),{' ', '\0'});
+% fid = fopen(strfn);
+% Header = fread(fid, [1, 124], 'char');
+% fclose(fid);
+Fl = memmapfile(strfn);
+Header = char(Fl.Data(1:64))';
+strh = strsplit(Header,{' ', '\0'});
+
+%idx = 1:min(length(Header), 100); % 
+%strh = strsplit(Header(idx),{' ', '\0'});
 
 datestr = [strh{5} ' ' strh{6}];
 
@@ -11,7 +16,6 @@ Dim = [str2double(strh{1}) str2double(strh{2}) str2double(strh{3})];
 Freq = str2double(strh{4});
 Type = strh{7};
 
-fclose(fid);
 
 disp(datestr)
 disp(['Dimensions : ' num2str(Dim)])
